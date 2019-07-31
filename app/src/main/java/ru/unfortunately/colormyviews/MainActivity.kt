88a7d76e45dc6ff80a2a7f55059ff0f6b18f8cab
DryFear <voronin8945@mail.ru>
2019-07-31 18:16:02 +0300

    It just works!

--- app/src/main/java/ru/unfortunately/colormyviews/MainActivity.kt
+++ app/src/main/java/ru/unfortunately/colormyviews/MainActivity.kt
@@ -1,12 +1,44 @@
 package ru.unfortunately.colormyviews
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        val clickableViews: List<View> = listOf(box_two_text, box_three_text, box_four_text, box_five_text, box_one_text,
+            button_green, button_red, button_yellow)
+        for (item in clickableViews){
+            item.setOnClickListener{makeColored(it)}
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.button_red -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.button_yellow -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.button_green -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
